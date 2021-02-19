72f8118a680255ef23d63319fe2a4f4427c5e569
rosakun <65656375+rosakun@users.noreply.github.com>
2021-02-19 21:16:53 +0100

    This is my first commit for part one of my neural nets project!

---
+++ README.md
@@ -0,0 +1,38 @@
+# FINAL BOSS: The last NN Project
+
+ This is the final project of my neural networks introductory course.
+It is a POS-Tagger that uses a simple neural network.
+
+### Table of Contents
+
+* General Information (#general-information)
+* Features (#features)
+* Setup (#setup)
+* Usage (#usage)
+* Preprocessing (#preprocessing)
+* Technologies (#technologies)
+* Sources (#source)
+* Other Information (#other-information)
+
+
+#### Preprocessing
+
+To preprocess your data, you will need the files 
+- data_preprocesser.py , which contains the code that preprocesses your data
+- your data in a .conll file, for example "sample.conll"
+
+Make sure to keep these files in the same directory.
+
+In your terminal, move to the directory where you have stored these files:
+
+	cd NN-Project
+
+To run the preprocesser, call it with the file you want to preprocess and the file to write the preprocessed data in as arguments: 
+
+	py data_preprocesser.py sample.conll sample.info
+
+
+
+#### Technologies
+
+Python 3.8.2
---
+++ data_preprocesser.py
@@ -0,0 +1,48 @@
+
+import re
+import sys
+
+
+def main():
+    with open(sys.argv[1]) as f:
+        with open("sample.tsv", "w+") as g:
+            for line in f:
+                if line[0] != '#':
+                    thing = line.split()
+                    if len(thing) > 0:
+                        g.write("\t".join(thing[2:5]) + "\n")
+    sequence_length = []
+    flag = -1
+    tagcount = {}
+    totalcount = 0
+    with open("sample.tsv") as f:
+        for line in f:
+            totalcount += 1
+            number = int(line.split()[0])
+            if int(flag) >= 0:
+                if number == 0:
+                    sequence_length.append(flag+1)
+            flag = number
+            tag = line.split()[2]
+            if tag not in tagcount:
+                tagcount[tag] = 1
+            elif tag in tagcount:
+                tagcount[tag] += 1
+    maxi = max(sequence_length)
+    mini = min(sequence_length)
+    mean = sum(sequence_length)/(len(sequence_length)+1)
+    infofile = open(sys.argv[2] , "w+")
+    infofile.write("Max sequence length: " + str(maxi) + "\n")
+    infofile.write("Min sequence length: " + str(mini) + "\n")
+    infofile.write("Mean sequence length: " + str(mean) + "\n")
+    infofile.write("Number of sequences: " + str(len(sequence_length)+1) + "\n" + "\n")
+    infofile.write("Tags:" + "\n")
+    prozente = 0
+    for item in tagcount:
+        prozentwert = tagcount[item]/totalcount
+        prozente += prozentwert
+        infofile.write(item + "\t" + str(prozentwert) + "%" + "\n")
+
+
+if __name__ == "__main__":
+    main()
